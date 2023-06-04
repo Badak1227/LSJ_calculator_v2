ad08fd49632c11cf1f6ea89cc724e4b9a718594e
Badak <dltmdwls122789@gmail.com>
2023-06-04 21:45:58 +0900

    Add - log10, log2 calculatae - decimal num input

--- calc.c
+++ calc.c
@@ -5,6 +5,7 @@
 
 int calculate(stack* operand, stack* operator, int n) {
 	int error = 0;
+
 	while (!isEmptyStack(operator)) {
 
 		char sign = pop(operator);
@@ -50,6 +51,31 @@ int calculate(stack* operand, stack* operator, int n) {
 			break;
 		}
 	}
+
+	if (!isEmptyStack(operator)) {
+		if (top(operator) == '1') {
+			pop(operator);
+
+			int num = pop(operand);
+
+			if (num < 1) {
+				printf("ERROR: log10(n), n Less Than 1");
+				error = 2;
+			}
+			push(log10(num), operand);
+		}
+		else if (top(operator) == '2') {
+			pop(operator);
+
+			int num = pop(operand);
+			if (num < 1) {
+				printf("ERROR: log2(n), n Less Than 1");
+				error = 2;
+			}
+			push(log2(num), operand);
+		}
+	}
+
 	return error;
 }
 
@@ -59,6 +85,8 @@ void calc(queue* exp) {
 	stack operator = newStack(); // 연산자 스택
 
 	double num = 0;
+
+	double point = 0; //소수점 체크
 
 	int preNumCheck = 0; // 이전에 숫자가 왔는지 체크
 
@@ -73,13 +101,89 @@ void calc(queue* exp) {
 	while (!isEmptyQueue(exp) && errorCode == 0) {
 		char cur = deQueue(exp);
 
-		if ('0' <= cur && cur <= '9') {
-			if (closeCheck == 1) {
+		if (cur == '.') {
+			if (preNumCheck == 0 || point > 0) {
 				errorCode = 1;
 				break;
 			}
 
-			num = num * 10 + (cur - '0');
+			point = 1;
+		}
+		else if (cur == '+') {
+			if (preNumCheck == 0) {
+				if (plusCheck == 1) {
+					errorCode = 1;
+					break;
+				}
+
+				push(1, &operand);
+				push('*', &operator);
+			}
+			else {
+				push(num, &operand);
+				num = 0;
+
+				if (!isEmptyStack(&operator)) {
+					char prev = top(&operator);
+					if (prev == '-' || prev == '+' || prev == '*' || prev == '/' || prev == '%' || prev == '^') {
+						errorCode = calculate(&operand, &operator, 1);
+					}
+				}
+
+
+				push(cur, &operator);
+				preNumCheck = 0;
+				closeCheck = 0;
+			}
+
+			point = 0;
+			plusCheck = 1;
+			minusCheck = 0;
+		}
+		else if (cur == '-') {
+			if (preNumCheck == 0) {
+				if (minusCheck == 1) {
+					errorCode = 1;
+					break;
+				}
+
+				push(-1, &operand);
+				push('*', &operator);
+			}
+			else {
+				push(num, &operand);
+				num = 0;
+
+				if (!isEmptyStack(&operator)) {
+					char prev = top(&operator);
+					if (prev == '-' || prev == '+' || prev == '*' || prev == '/' || prev == '%' || prev == '^') {
+						errorCode = calculate(&operand, &operator, 1);
+					}
+				}
+
+				push(cur, &operator);
+				preNumCheck = 0;
+				
+				closeCheck = 0;
+			}
+
+			point = 0;
+			plusCheck = 0;
+			minusCheck = 1;
+		}
+		else if ('0' <= cur && cur <= '9') {
+			if (closeCheck == 1) {
+				errorCode = 1;
+				break;
+			}
+			
+			if (point > 0) {
+				point *= 10;
+				num = num + (cur - '0')/ point;
+			}
+			if (point == 0) {
+				num = num * 10 + (cur - '0');
+			}
 
 			preNumCheck = 1;
 			minusCheck = 0;
@@ -87,68 +191,7 @@ void calc(queue* exp) {
 			closeCheck = 0;
 		}
 		else {
-			if (cur == '-') {
-				if (preNumCheck == 0) {
-					if (minusCheck == 1) {
-						errorCode = 1;
-						break;
-					}
-
-					push(-1, &operand);
-					push('*', &operator);
-					minusCheck = 1;
-				}
-				else {
-					push(num, &operand);
-					num = 0;
-
-					if (!isEmptyStack(&operator)) {
-						char prev = top(&operator);
-						if (prev == '-' || prev == '+' || prev == '*' || prev == '/' || prev == '%' || prev == '^') {
-							errorCode = calculate(&operand, &operator, 1);
-						}
-					}
-
-					push(cur, &operator);
-					preNumCheck = 0;
-					minusCheck = 1;
-					closeCheck = 0;
-				}
-
-				plusCheck = 0;
-			}
-			else if (cur == '+') {
-				if (preNumCheck == 0) {
-					if (plusCheck == 1) {
-						errorCode = 1;
-						break;
-					}
-
-					push(1, &operand);
-					push('*', &operator);
-					plusCheck = 1;
-				}
-				else {
-					push(num, &operand);
-					num = 0;
-
-					if (!isEmptyStack(&operator)){
-						char prev = top(&operator);
-						if (prev == '-' || prev == '+' || prev == '*' || prev == '/' || prev == '%' || prev == '^') {
-							errorCode = calculate(&operand, &operator, 1);
-						}
-					}
-
-					
-					push(cur, &operator);
-					preNumCheck = 0;
-					plusCheck = 1;
-					closeCheck = 0;
-				}
-
-				minusCheck = 0;
-			}
-			else if (cur == '*' || cur == '/' || cur == '%') {
+			if (cur == '*' || cur == '/' || cur == '%') {
 				if (preNumCheck == 0) {
 					errorCode = 1;
 					break;
@@ -166,8 +209,6 @@ void calc(queue* exp) {
 				push(cur, &operator);
 				
 				preNumCheck = 0;
-				plusCheck = 0;
-				minusCheck = 0;
 				closeCheck = 0;
 			}
 			else if (cur == '^') {
@@ -180,10 +221,7 @@ void calc(queue* exp) {
 				num = 0;
 				push(cur, &operator);
 
-
 				preNumCheck = 0;
-				plusCheck = 0;
-				minusCheck = 0;
 				closeCheck = 0;
 			}
 			else if (cur == '(') {
@@ -193,9 +231,6 @@ void calc(queue* exp) {
 				}
 				
 				push('(', &operator);
-
-				plusCheck = 0;
-				minusCheck = 0;
 			}
 			else if (cur == ')') {
 				if (preNumCheck == 0) {
@@ -208,14 +243,67 @@ void calc(queue* exp) {
 				errorCode = calculate(&operand, &operator, 0);
 
 				num = pop(&operand); // 마지막에 숫자를 스택에 넣어주는 형태를 만들어 주기 위함
-
-				plusCheck = 0;
-				minusCheck = 0;
+				
 				closeCheck = 1;
 			}
+			else if (cur == 'l') {
+				errorCode = 1;
+
+				if (preNumCheck == 1) break;
+
+				if (isEmptyQueue(exp)) break;
+				cur = deQueue(exp);
+
+				if (cur == 'o') {
+					if (isEmptyQueue(exp)) break;
+					cur = deQueue(exp);
+
+					if (cur == 'g') {
+						if (isEmptyQueue(exp)) break;
+						cur = deQueue(exp);
+
+						if (cur == '1') {
+							if (isEmptyQueue(exp)) break;
+							cur = deQueue(exp);
+
+							if (cur == '0') {
+								if (isEmptyQueue(exp)) break;
+								cur = deQueue(exp);
+
+								if (cur == '(') {
+									
+									push('1', &operator);
+									push('(', &operator);
+
+									errorCode = 0;
+								}
+							}
+						}
+						else if (cur == '2') {
+							if (isEmptyQueue(exp)) break;
+							cur = deQueue(exp);
+
+							if (cur == '(') {
+								
+								push('2', &operator);
+								push('(', &operator);
+
+								errorCode = 0;
+							}
+						}
+					}
+				}
+			}
+			else {
+				errorCode = 1;
+				break;
+			}
+
+			point = 0;
+			plusCheck = 0;
+			minusCheck = 0;
 		}
 	}
-
 	// 마지막이 숫자로 끝나지 않았을 시 체크, ')'로 끝나도 숫자로 끝나는 것으로 취급
 	if (preNumCheck == 0) {
 		errorCode = 1;
@@ -225,22 +313,17 @@ void calc(queue* exp) {
 	if (errorCode == 1) {
 		printf("ERROR: Invalid Experiment\n");
 	}
+	else if (errorCode == 2) {
+
+	}
 	else {
 		push(num, &operand);
 
 		errorCode = calculate(&operand, &operator, 0);
 
-		//계산 과정 오류 체크
 		if (errorCode == 0) {
 			double res = top(&operand);
-
-			//소수점 첫째 자리까지 표현, 첫째자리가 0일 경우 소수점 표현 생략
-			if (fmod(res * 10, 10) == 0) {
-				printf("result: %.0lf\n", res);
-			}
-			else {
-				printf("result: %.1lf\n", res);
-			}
+			printf("result: %.2lf\n", round(res*100)/100);
 		}
 	}
 
@@ -263,7 +346,7 @@ int input() {
 			continue;
 		}
 		//괄호, 숫자, 연산자 외 입력 검사
-		if (('0' <= cur && cur <= '9') || cur == '(' || cur == ')' || cur == '+' || cur == '-' || cur == '*' || cur == '/' || cur == '%' || cur == '^') {
+		if (('0' <= cur && cur <= '9') || cur == '(' || cur == ')' || cur == '+' || cur == '-' || cur == '*' || cur == '/' || cur == '%' || cur == '^' || cur == '!' || cur == '~' || cur == '.' || cur == 'l' || cur == 'o' || cur == 'g') {
 
 			//괄호쌍 검사
 			if (cur == '(') {
